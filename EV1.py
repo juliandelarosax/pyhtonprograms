56192d5fb8858aedd5afb68e835b01dea9b2ed14
juliandelarosax <145611918+juliandelarosax@users.noreply.github.com>
2023-09-20 22:52:04 -0600

    Add files via upload

---
+++ EV1.py
@@ -0,0 +1,9 @@
+n1=int(input("escriba el primer numero"))
+n2=int(input("escriba el segundo numero"))
+operacion=input("escriba la operacion que desea realizar")
+if operacion== "suma":
+    print("el resultado de la suma es: ",n1+n2)
+if operacion== "resta":
+    print("el resultado de la resta es: ",n1-n2)
+if operacion== "division":
+    print("el resultado de la division es: ",n1/n2)
